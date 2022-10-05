7504401b352abf0527ec86b3645baa2cabaab8eb
Wojciech Knopp <w.knopp.132@studms.ug.edu.pl>
2022-10-05 11:19:50 +0000

    Upload New File

---
+++ Lab01/zadanie4/Main.java
@@ -0,0 +1,21 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        do{
+            System.out.print("Podaj liczbę naturalną: ");
+            try{
+                n = input.nextInt();
+            }catch(InputMismatchException e){
+                String trash = input.next(); //Naprawia zepsuty input po exception
+                n = -1;
+            }
+            if(n < 1) System.out.println("To nie jest liczba naturalna!");
+        }while(n < 1);
+
+        Wzor.narysuj(n);
+    }
+}
