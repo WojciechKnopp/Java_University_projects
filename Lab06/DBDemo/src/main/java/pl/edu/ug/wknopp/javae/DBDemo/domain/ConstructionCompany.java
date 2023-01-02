0bb393faafbf7f34eb76dc31ad05f96177f61cc9
Wojtek <umboj2000@gmail.com>
2023-01-02 13:43:26 +0100

    dodanie brakującej kolekcji postman z lab09 i JsonIgnore dla Company

--- Lab06/DBDemo/src/main/java/pl/edu/ug/wknopp/javae/DBDemo/domain/ConstructionCompany.java
+++ Lab06/DBDemo/src/main/java/pl/edu/ug/wknopp/javae/DBDemo/domain/ConstructionCompany.java
@@ -1,8 +1,11 @@
 package pl.edu.ug.wknopp.javae.DBDemo.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
+@JsonIgnoreProperties("houses")
 @Entity
 public class ConstructionCompany {
 
